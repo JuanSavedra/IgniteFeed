bb62746ce5b5f5eb2b593ec2b163349c929ac904
Juan Savedra dos Santos <juansavedra827@gmail.com>
2022-06-21 13:54:29 -0300

    sidebar (finished)

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -8,11 +9,18 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1584949091598-c31daaaa4aa9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
+        <img 
+          className={styles.avatar}
+          src="https://github.com/JuanSavedra.png" 
+        />
         <strong>Juan Savedra</strong>
         <span>Web Developer</span>
       </div>
       <footer>
         <a href="#">
+          <PencilLine 
+            size={20} 
+          />
           Editar seu perfil
         </a>
       </footer>
