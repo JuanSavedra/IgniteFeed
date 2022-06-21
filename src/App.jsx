a00328d308c7988295ed32e5ce62459919066a25
Juan Savedra dos Santos <juansavedra827@gmail.com>
2022-06-21 13:30:47 -0300

    sidebar (WIP)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
+import styles from './App.module.css';
 import { Post } from './Post';
-import { Header } from './components/Header'
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 import './global.css';
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Juan Savedra"
-        content="Lorem Ipsum"
-      />
-      <Post 
-        author="Vitor Savedra"
-        content="Lorem Ipsum"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Juan Savedra"
+            content="Lorem Ipsum"
+          />
+          <Post 
+            author="Vitor Savedra"
+            content="Lorem Ipsum"
+          />
+        </main>
+      </div>
     </div>
   )
 }
