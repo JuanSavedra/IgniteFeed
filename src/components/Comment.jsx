2a1e04952b2bee6d02e2d14fe704a9f2bd2b3955
Juan Savedra dos Santos <juansavedra827@gmail.com>
2022-06-22 16:30:29 -0300

    comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import styles from './Comment.module.css';
+import { ThumbsUp, Trash } from 'phosphor-react';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/JuanSavedra.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}> 
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Juan Savedra</strong>
+              <time 
+                title="22 de junho às 14:00h" 
+                dateTime="2022-06-22 14:00:00"
+              >
+                Publicado há 1h
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp size={20}/>
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
