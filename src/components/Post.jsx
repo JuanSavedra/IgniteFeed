24bce91595300df21582bf2e179e7feedb4c26db
Juan Savedra dos Santos <juansavedra827@gmail.com>
2022-06-23 15:22:43 -0300

    iteration

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,7 +2,9 @@ import styles from './Post.module.css';
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 
-export function Post() {
+export function Post(props) {
+  console.log(props);
+
   return (
     <article className={styles.post}>
       <header>
@@ -23,17 +25,7 @@ export function Post() {
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, 
-          evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>👉{' '}<a href="#">jane.design/doctorcare</a></p>
-        <p>
-          <a href="#">#novoprojeto</a>{' '} 
-          <a href="#">#nlw</a>{' '}
-          <a href="#">#rocketseat</a>
-        </p>
+        
       </div>
 
       <form className={styles.commentForm}>
@@ -45,8 +37,6 @@ export function Post() {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
         <Comment />
       </div>
     </article>
