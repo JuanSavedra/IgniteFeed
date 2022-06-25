6ec25315e99309c6c938a66164f56d750438794f
Juan Savedra dos Santos <juansavedra827@gmail.com>
2022-06-25 13:16:38 -0300

    communication between components

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,11 @@ import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 import { ThumbsUp, Trash } from 'phosphor-react';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/JuanSavedra.png" />
@@ -18,7 +22,7 @@ export function Comment({ content }) {
                 Publicado há 1h
               </time>
             </div>
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
